f2438c228118ac8d1488e0cff733d361c7363208
OttoPekkaOjala <s2419922@edu.savonia.fi>
2026-03-30 14:24:26 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace tuotanto1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+            /// muutos
+            /// uus
+        }
+    }
+}
