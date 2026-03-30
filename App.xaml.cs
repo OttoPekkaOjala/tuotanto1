d7aa5a21b07100bfed261ffad008938c47c6167b
OttoPekkaOjala <s2419922@edu.savonia.fi>
2026-03-30 15:14:23 +0300

    uus

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,8 +7,11 @@
             InitializeComponent();
 
             MainPage = new AppShell();
+<<<<<<< HEAD
             /// muutos
             /// uus
+=======
+>>>>>>> b5b354666e3996511565ccddb650af3ac1cf34d7
         }
     }
 }
