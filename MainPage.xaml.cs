951a6d32bffdc1f5e444607f6b870d2d576a7494
jm1237 <sinun.sahkoposti@esimerkki.com>
2026-04-14 11:26:42 +0300

    sql

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -10,16 +10,13 @@
         public MainPage()
         {
             InitializeComponent();
-<<<<<<< HEAD
             /// Muutos
             /// toinen
         }
 
 
-=======
         }
 
->>>>>>> b5b354666e3996511565ccddb650af3ac1cf34d7
         private void OnCounterClicked(object sender, EventArgs e)
         {
             count++;
