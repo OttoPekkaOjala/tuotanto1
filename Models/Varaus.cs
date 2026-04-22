6e04568fa4ad9bf719352c42282978a6442aeffa
eepojoki <s2520462@edu.savonia.fi>
2026-04-22 15:44:53 +0300

    muutoos

--- Models/Varaus.cs
+++ Models/Varaus.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace tuotanto1.Models;
-
-public class Varaus
+﻿namespace tuotanto1.Models
 {
-    public int VarausId { get; set; }
-    public int AsiakasId { get; set; }
-    public int MokkiId { get; set; }
-    public DateTime VarattuPvm { get; set; }
-    public DateTime VahvistusPvm { get; set; }
-    public DateTime VarattuAlkuPvm { get; set; }
-    public DateTime VarattuLoppuPvm { get; set; }
+    public class Varaus
+    {
+        public int VarausId { get; set; }
+        public int AsiakasId { get; set; }
+        public int MokkiId { get; set; }
+        public DateTime Alkupvm { get; set; }
+        public DateTime Loppupvm { get; set; }
+        public decimal Hinta { get; set; }
+    }
 }
--- ViewModels/VarausViewModel.cs
+++ ViewModels/VarausViewModel.cs
@@ -1,12 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using tuotanto1.Models;
 
 namespace tuotanto1.ViewModels
 {
-    internal class VarausViewModel
+    public class VarausViewModel
     {
+        public ObservableCollection<Varaus> Varaukset { get; set; }
+
+        public VarausViewModel()
+        {
+            Varaukset = new ObservableCollection<Varaus>
+            {
+                new Varaus
+                {
+                    VarausId = 1,
+                    AsiakasId = 1,
+                    MokkiId = 3,
+                    Alkupvm = new DateTime(2024, 6, 10),
+                    Loppupvm = new DateTime(2024, 6, 15),
+                    Hinta = 550.00m
+                },
+                new Varaus
+                {
+                    VarausId = 2,
+                    AsiakasId = 2,
+                    MokkiId = 1,
+                    Alkupvm = new DateTime(2024, 7, 1),
+                    Loppupvm = new DateTime(2024, 7, 5),
+                    Hinta = 420.00m
+                },
+                new Varaus
+                {
+                    VarausId = 3,
+                    AsiakasId = 1,
+                    MokkiId = 2,
+                    Alkupvm = new DateTime(2024, 8, 12),
+                    Loppupvm = new DateTime(2024, 8, 20),
+                    Hinta = 890.00m
+                }
+            };
+        }
     }
 }
