7e14e9f6bc34d1e47c9adde5fd7bea7e7ba2a97f
eepojoki <s2520462@edu.savonia.fi>
2026-04-22 15:23:55 +0300

    jyy

--- Views/VarausPage.xaml.cs
+++ Views/VarausPage.xaml.cs
@@ -2,8 +2,27 @@ namespace tuotanto1.Views;
 
 public partial class VarausPage : ContentPage
 {
-	public VarausPage()
-	{
-		InitializeComponent();
-	}
+    public VarausPage()
+    {
+        InitializeComponent();
+        BindingContext = new ViewModels.VarausViewModel();
+    }
+
+    private async void Etusivu_Clicked(object sender, EventArgs e)
+        => await Navigation.PushAsync(new MainPage());
+
+    private async void Asiakkaat_Clicked(object sender, EventArgs e)
+        => await Navigation.PushAsync(new AsiakasPage());
+
+    private async void Mokit_Clicked(object sender, EventArgs e)
+        => await Navigation.PushAsync(new MokkiPage());
+
+    private async void Varaukset_Clicked(object sender, EventArgs e)
+        => await Navigation.PushAsync(new VarausPage());
+
+    private async void Laskut_Clicked(object sender, EventArgs e)
+        => await Navigation.PushAsync(new LaskuPage());
+
+    private async void Raportit_Clicked(object sender, EventArgs e)
+        => await Navigation.PushAsync(new RaportitPage());
 }
